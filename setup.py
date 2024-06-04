771bcc720b31d4a79c05b459fe811c676fbb166d
123rc <chakrabortyrohit181@gmail.com>
2024-06-04 13:57:46 +0530

    C:/Program Files/Git/folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='hand_sign_detection',
+    version='0.0.0',
+    author='rohit',
+    author_email='chakrabortyrohit181@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)

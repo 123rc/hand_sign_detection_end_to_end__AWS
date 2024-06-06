170f26f3dbc5fd9d6129f09b4dc464bf7a3a6236
123rc <chakrabortyrohit181@gmail.com>
2024-06-06 23:20:29 +0530

    changes

--- hand_sign_detection/constant/__init__.py
+++ hand_sign_detection/constant/__init__.py
@@ -1,9 +1,9 @@
 ARTIFACTS_DIR = 'artifacts'
 
-DATA_INGESTION_DIR:str= 'data_ingestion'
-DATA_INGESTION_FEATURES_DIR:str= 'feature_store'
+DATA_INGESTION_DIR_NAME:str= 'data_ingestion'
+DATA_INGESTION_FEATURE_STORE_DIR:str= 'feature_store'
 
 
 
-DATA_DOWNLOAD_DIR:str= "https://drive.google.com/file/d/1i_0BzRffZ_gMqNNQkNKPdeqbORFdhHg-/view?usp=sharing"
+DATA_DOWNLOAD_URL:str= "https://drive.google.com/file/d/1i_0BzRffZ_gMqNNQkNKPdeqbORFdhHg-/view?usp=sharing"
 

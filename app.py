cfd3684ea0856c79a6d1be4a65aff62f57e15cc8
123rc <chakrabortyrohit181@gmail.com>
2024-06-11 23:23:44 +0530

    changes

--- app.py
+++ app.py
@@ -1,4 +1,77 @@
-from hand_sign_detection.pipeline.training_pipeline import TrainPipeline
+import os,sys
 
-obj=TrainPipeline()
-obj.run_pipeline()
+from flask import Flask, Response, jsonify, render_template, request
+from flask_cors import CORS, cross_origin
+from hand_sign_detection.pipeline.training_pipeline import TrainPipeline
+from hand_sign_detection.logger import logging
+from hand_sign_detection.exception import AppException
+from hand_sign_detection.entity.artifacts_entity import DataIngestionArtifacts, DataValidationArtifact, ModelTrainerArtifact
+from hand_sign_detection.utils.main_utils import decodeImage, encodeImageIntoBase64
+from hand_sign_detection.constant.application import APP_HOST, APP_PORT
+
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.jpg"
+
+@app.route("/train")
+def trainRoute():
+    obj = TrainPipeline()
+    obj.run_pipeline()
+    return "Training Successfull!!" 
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=['POST','GET'])
+@cross_origin()
+def predictRoute():
+    try:
+        image = request.json['image']
+        decodeImage(image, clApp.filename)
+
+        os.system("cd yolov5/ && python detect.py --weights best.pt --img 416 --conf 0.5 --source ../data/inputImage.jpg")
+
+        opencodedbase64 = encodeImageIntoBase64("yolov5/runs/detect/exp/inputImage.jpg")
+        result = {"image": opencodedbase64.decode('utf-8')}
+        os.system("rm -rf yolov5/runs")
+
+    except ValueError as val:
+        print(val)
+        return Response("Value not found inside  json data")
+    except KeyError:
+        return Response("Key value error incorrect key passed")
+    except Exception as e:
+        print(e)
+        result = "Invalid input"
+
+    return jsonify(result)
+
+
+
+@app.route("/live", methods=['GET'])
+@cross_origin()
+def predictLive():
+    try:
+        os.system("cd yolov5/ && python detect.py --weights best.pt --img 416 --conf 0.5 --source 0")
+        os.system("rm -rf yolov5/runs")
+        return "Camera starting!!" 
+
+    except ValueError as val:
+        print(val)
+        return Response("Value not found inside  json data")
+    
+
+
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+    app.run(host=APP_HOST, port=APP_PORT)
